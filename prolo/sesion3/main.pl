7b32e2ff19d76eabee14c2fdc6c8dd4703fb5721
DHurtado714-itesm <a01707774@itesm.mx>
2023-03-16 16:52:16 -0600

    dfs

--- prolo/sesion3/main.pl
+++ prolo/sesion3/main.pl
@@ -19,6 +19,8 @@ quick_sort([Head | Tail], Sorted) :-
 empty_stack([]).
 push_stack(X, Stack, [X | Stack]).
 
+%% arc(Origin, Destination).
+
 arc(1, 2).
 arc(1, 3).
 arc(1, 4).
@@ -35,3 +37,23 @@ arc(4, 11).
 arc(7, 8).
 arc(7, 10).
 arc(8, 9).
+
+%% conection(A, B) :- arc(A, B); arc(B, A).
+
+dfs(Start, Goal, Path) :-
+  push_stack(Start, [], Visited),
+  path(Start, Goal, Visited).
+
+%% Path(Current, Goal, Visited).
+path(Goal, Goal, _),
+path(Current, Goal, Visited) :-
+  arc(Current, Next),
+  not(member(Current, Next)),
+  push_stack(Next, Visited, NewVisited),
+  path(Next, Goal, NewVisited).
+
+
+
+
+
+
